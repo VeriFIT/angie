42641ca0d4e2aa5129828f220b9fc166e4343952
Michal Kotoun <xkotou04@stud.fit.vutbr.cz>
2017-04-18 02:40:35 +0000

    Smg: Wrappers: use transfrom, remove obsolete code
    TODO: create a new wrapper code to get type-info-help back

--- src/Smg/Wrappers.hpp
+++ src/Smg/Wrappers.hpp
@@ -10,13 +10,6 @@ class ISmgVisitor;
 
 namespace Smg {
 
-////class ValueWrapper {
-////  ValueId id;
-////  Type type;
-////  IValueContainer& vc;
-////
-////  ValueWrapper operator+(const ValueWrapper& rhs) { vc.Add(id, rhs.id, type,  }
-////}
 
 template <class OutT, class ViewT>
 auto /*ObjectSeo<ViewT, OutT>*/ objseo(ViewT && rng, Impl::Graph& graph)
@@ -34,116 +27,6 @@ public:
   Graph(Impl::Graph& graph) : graph{graph} {}
 };
 
-class SmgSet{
-  Impl::Graph& graph;
-public:
-  SmgSet(decltype(graph) graph) : graph{graph} {}
-  auto GetGraph() const -> decltype(graph) { return graph; }
-};
-
-template <class ViewT, class OutT>
-class ObjectSeo : public SmgSet, public ranges::view_adaptor<ObjectSeo<ViewT, OutT>, ViewT> {
-private:
-  friend ranges::range_access;
-  class adaptor : public ranges::adaptor_base
-  {
-    Impl::Graph* const graph;
-  public:
-    adaptor() = default;
-    adaptor(Impl::Graph& g) : graph{&g} {};
-
-    static_assert(sizeof(decltype(&ranges::adaptor_base::get(ranges::range_iterator_t<ViewT>{}))) != 0, "we are hiding method get");
-    OutT get(ranges::range_iterator_t<ViewT> it) const {
-      return {*it, *graph};
-    }
-  };
-  adaptor begin_adaptor() const { return {GetGraph()}; }
-  adaptor end_adaptor()   const { return {GetGraph()}; } 
-public:
-  ObjectSeo() = default;
-  ObjectSeo(ViewT&& rng, Impl::Graph& context)
-    : ObjectSeo::view_adaptor{std::forward<ViewT>(rng)}
-    , SmgSet(context)
-  {}
-};
-
-template <class ViewT>
-class ObjectSet : public SmgSet, public ranges::view_adaptor<ObjectSet<ViewT>, ViewT> {
-private:
-  friend ranges::range_access;
-public:
-  typename std::remove_reference<ViewT>::type::iterator begin()
-  {
-    return ranges::view_adaptor<ObjectSet<ViewT>, ViewT>::base().begin();
-  }
-  typename std::remove_reference<ViewT>::type::iterator end()
-  {
-    return ranges::view_adaptor<ObjectSet<ViewT>, ViewT>::base().end();
-  }
-  ObjectSet() = default;
-  ObjectSet(ViewT&& rng, Impl::Graph& context)
-    : ObjectSet::view_adaptor{std::forward<ViewT>(rng)}
-    , SmgSet(context)
-  {}
-};
-
-////
-////template <class ViewT>
-////class ObjectSet : public SmgSet, ranges::v3::view_facade<ObjectSet<ViewT>> {
-////private:
-////  friend ranges::v3::range_access;
-////
-////  ViewT& container;
-////
-////  struct cursor
-////  {
-////  private:
-////    ranges::range_iterator_t<ViewT> iter;
-////  public:
-////    cursor() = default;
-////    cursor(ranges::range_iterator_t<ViewT> it)
-////      : iter(it)
-////    {}
-////    int const & read() const
-////    {
-////      return *iter;
-////    }
-////    bool equal(cursor const &that) const
-////    {
-////      return iter == that.iter;
-////    }
-////    void next()
-////    {
-////      ++iter;
-////    }
-////    void prev()
-////    {
-////      --iter;
-////    }
-////    std::ptrdiff_t distance_to(cursor const &that) const
-////    {
-////      return that.iter - iter;
-////    }
-////    void advance(std::ptrdiff_t n)
-////    {
-////      iter += n;
-////    }
-////  };
-////  cursor begin_cursor() const
-////  {
-////    return {container.begin()};
-////  }
-////  cursor end_cursor() const
-////  {
-////    return {container.end()};
-////  }
-////  public:
-////  ObjectSet() = default;
-////  ObjectSet(ViewT& rng, Graph& context)
-////    : container{rng}
-////    , SmgSet(context)
-////  {}
-////};
 class Object {
 protected:
   Impl::Object& object;
